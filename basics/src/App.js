c093e11a069ee2de792b887c0872c6d30f870cd9
Priyansh Sahu <priyansh.sahu25@gmail.com>
2024-02-14 11:48:17 +0000

    commit

--- basics/src/App.js
+++ basics/src/App.js
@@ -1,13 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1> Hello </h1>
-      </header>
+    <h1>Welcome to React</h1>
     </div>
   );
 }
